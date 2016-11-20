5b058fbcc0be0606885aa9b59bf79a29067f37e4
Unknown <lauriane.l@gmail.com>
2016-11-20 04:05:51 +0100

    style

--- index.php
+++ index.php
@@ -3,12 +3,20 @@
 <head>
 <meta charset="utf-8" />
 <link href="style.css" rel="stylesheet"/>
+<script src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.16/webfont.js"></script>
+<script>
+  WebFont.load({
+    google: {
+      families: ['Archivo Black']
+    }
+  });
+</script>
 </head>
 
 <body>
 
 <form>
-<input autofocus type="text" name="search"></input>
+<input type="text" name="search"></input>
 <br />
 
 <?php
