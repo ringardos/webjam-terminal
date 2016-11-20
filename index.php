3731b05a7df4e2c2118c9930bbbde78cabfb6846
Leo Catonnet <leo.catonnet@gmail.com>
2016-11-20 02:13:36 +0100

    explode slug + boucle

--- index.php
+++ index.php
@@ -9,25 +9,24 @@
 
 <form>
 <input autofocus type="text" name="search"></input>
+<br />
 
-<?php 
-$content = $_GET['search'];
-
-
-$data = json_decode(file_get_contents('http://api.giphy.com/v1/gifs/search?q='.$content.'&limit=1&api_key=dc6zaTOxFJmzC'));
-
-//var_dump($data);
-
-foreach($data->data as $gif){
-    echo($gif->slug.'<br/>');
+<?php
+if(isset($_GET['search'])){
+  $content = $_GET['search'];
+  $data = json_decode(file_get_contents('http://api.giphy.com/v1/gifs/search?q='.$content.'&limit=1&api_key=dc6zaTOxFJmzC'));
+  //var_dump($data);
+  $gif=$data->data[0];
+  echo('<img src="'.$gif->images->original->url.'" /><br />');
+  for ($i=0; $i < 5 ; $i++) {
+    $content = explode('-',$gif->slug)[0];
+    echo($content.'<br/>');
+    $data = json_decode(file_get_contents('http://api.giphy.com/v1/gifs/search?q='.$content.'&limit=1&api_key=dc6zaTOxFJmzC'));
+    $gif=$data->data[0];
     echo('<img src="'.$gif->images->original->url.'" /><br />');
+  }
 }
 
-$content2 = $_GET[$gif->slug];
-
-$data2 = json_decode(file_get_contents('http://api.giphy.com/v1/gifs/search?q='.$content2.'&limit=1&api_key=dc6zaTOxFJmzC'));
-
-var_dump($data2);
 
 //foreach($data2->data2 as $gif){
 //    echo($gif->slug.'<br/>');
@@ -54,7 +53,7 @@ var_dump($data2);
           }
       }
       public function __call($method, $arguments) {
-          $arguments = array_merge(array("stdObject" => $this), $arguments); 
+          $arguments = array_merge(array("stdObject" => $this), $arguments);
           if (isset($this->{$method}) && is_callable($this->{$method})) {
               return call_user_func_array($this->{$method}, $arguments);
           } else {
